0f659872233c27d792ac5c6e9ff1aaa2c15a2971
Caleb Thorsteinson <caleb@thorsteinson.me>
2016-03-10 22:25:48 -0800

    Added webpack configuration and deps

---
+++ webpack.production.js
@@ -0,0 +1,32 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: [
+    './index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      PRODUCTION: true
+    }),
+    new webpack.NoErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+      include: __dirname
+    }, {
+      test: /\.css?$/,
+      loaders: ['style', 'raw'],
+      include: __dirname
+    }]
+  }
+}
